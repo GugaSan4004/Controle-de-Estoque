c713785437a7b035b7c5bf9ddbccbe297a584a9f
GugaSan4004 <guga.4004@hotmail.com>
2026-07-04 07:30:53 -0300

    Start codding the html base template of the web-controller

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
 from modules.DBCore import start as DBCore
-from modules.WebmailCore import start as WebmailCore
+# from modules.WebmailCore import start as WebmailCore
 # from modules.EmailParsers import start as EmailParsers
 # from __temp.modules.EFManager import start as EFManager
-from modules.PDFManipulator import start as PDFManipulator
+# from modules.PDFManipulator import start as PDFManipulator
